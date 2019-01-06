caeeaae8d0ea56e479983023243df441d603f771
Samuel <bouaroua.samuel@gmail.com>
2019-01-06 14:42:03 +0100

    debug  vehicule selection, tidy simplify functions

--- js/app.js
+++ js/app.js
@@ -26,33 +26,20 @@ var crafts = [{
 	"direction": "North",
 	"blackBox": ["Launch from Earth", "Landing on Mars"],
 	"position": {
-		"x": 0,
-		"y": 0
+		"x": 1,
+		"y": 1
 	}
 },
 ];
 
-
-// wish to get data from a json file but not working..
-
-// var craftsfromJSON = jQuery.getJSON( "../data.json", function( data ) {
-// 	console.log("data from file" + data);
-// 	console.log("data from js" + crafts);
-// 	return data;
-// });
-// console.log("readed data from getJSON" + craftsfromJSON);
-
-
-// libs
-var hulla;
+// declaring libs and app vars
+var currentRover;
 var d3;
+var hulla;
 
-// user is connected to no Rover by default
-var currentRover = null;
-
-// trigger events when a Rover is selected (to turn it green) or red when turned offline.
-var event = new Event('roverSelection');
-var event2 = new Event('roverDisconnected');
+// events about Rover uplink with user, green for online, red for offline.
+var event = new Event('roverOn');
+var event2 = new Event('roverOff');
 
 // **************************//
 // ********* MOVES ********* //
@@ -97,81 +84,126 @@ function turnRight(vehicule) {
 function moveForward(vehicule) {
 
 	if (vehicule.direction === "North") {
-		vehicule.position.x += 0;
-		vehicule.position.y -= 1;
+		vehicule.position.y--;
 	} else if (vehicule.direction === "East") {
-		vehicule.position.x += 1;
-		vehicule.position.y += 0;
+		vehicule.position.x++;
 	} else if (vehicule.direction === "South") {
-		vehicule.position.x += 0;
-		vehicule.position.y += 1;
+		vehicule.position.y++;
 	} else {
-		vehicule.position.x -= 1;
-		vehicule.position.y += 0;
+		vehicule.position.x--;
 	}
 
 	// TO DRY
 	let msg = vehicule.name + " current position is [" + vehicule.position.x + " " + vehicule.position.y + "] after moving forward on your input.";
 	console.log(msg);
 
-	moveCircle(vehicule._id, vehicule.position.x, vehicule.position.y);
+	moveCircle(vehicule.position.x, vehicule.position.y);
 }
 
 function moveBackward(vehicule) {
 
 	if (vehicule.direction === "North") {
-		vehicule.position.x += 0;
-		vehicule.position.y += 1;
+		vehicule.position.y++;
 	} else if (vehicule.direction === "East") {
-		vehicule.position.x -= 1;
-		vehicule.position.y += 0;
+		vehicule.position.x--;
 	} else if (vehicule.direction === "South") {
-		vehicule.position.x += 0;
-		vehicule.position.y -= 1;
+		vehicule.position.y--;
 	} else {
-		vehicule.position.x += 1;
-		vehicule.position.y += 0;
+		vehicule.position.x++;
 	}
 
 	// TO DRY
 	let msg = vehicule.name + " current position is [" + vehicule.position.x + " " + vehicule.position.y + "] after moving backward on your input.";
 	console.log(msg);
 
-	moveCircle(vehicule._id, vehicule.position.x, vehicule.position.y);
+	moveCircle(vehicule.position.x, vehicule.position.y);
+}
+
+// find first available vehicule in the array (todo sort/filter) TODO if currentnull && none is active >> NO MORE VEHICULES
+function defaultVehicule(arr){
+	var found =	arr.find(function(element) {
+		return element.isActive;
+	});
+
+	// assign found vehicule to currentRover
+	currentRover = found;
+
+	// turn circle to green TODO precise circle selection for multiplayer
+	document.dispatchEvent(event);
+
+	// notify user
+	hulla.send("Connected to " +  found.name + "by default.. ", "info");
+	console.log("Connected to " +  found.name +  "by default.. ");
+	
+	return found;
 }
 
 // ======================
 // Focus commands on User chosen rover or disconnect
 
 function focusVehicule(arr, i) {
-	if ((arr === "disconnect") && (i == null)) {
-
-		// send log and notification
-		let msg = "User disconnected from " + currentRover.name + ".";
-		console.log(msg);
-		hulla.send(msg, "dark");
-
-		// back to no controlled rover
-		currentRover = null;
-
-	} else {
-		console.log("User connected to " + arr[i].name + ".");
+	// check if useful TODO
+	if (currentRover != null ){
 		currentRover = arr[i];
-		document.dispatchEvent(event);
+	} else {
+		currentRover = defaultVehicule(arr);
 	}
+
+	// turn it to green
+	document.dispatchEvent(event);
+
+	// notify user
+	hulla.send("Connected to " +  currentRover.name, "info");
+	
 	return currentRover;
 }
+
+function destroyVehicule() {
+
+	// send log and notification
+	console.log(currentRover.name + "  was destroyed ! ");
+	hulla.send(currentRover.name + "  was destroyed ! ", "danger");
+
+	console.log("User disconnected from " + currentRover.name + ".");
+	hulla.send("User disconnected from " + currentRover.name + ".", "dark");
+
+	// turn it red
+	document.dispatchEvent(event2);
+
+	// disable destroyed vehicule into selected array and set currentRover to none..
+	currentRover.isActive = false;
+
+	// set current vehicule to none
+	currentRover = null;
+	
+}
+
+// change focused rover appearance, green for online, red for offline
+document.addEventListener("roverOn", function () {
+	var onlineVehicule = d3.select( "#vehicule" + currentRover._id )
+		.classed("online-rover", true);
+}, false);
+
+document.addEventListener("roverOff", function () {
+	var offlineVehicule = d3.select( "#vehicule" + currentRover._id )
+		.classed("online-rover", false);
+}, false);
+
 
 // ======================
 // Processing valid User commands from text input (lrfb)	
 // Log sequence, user and timestamp into vehicule blackbox..
 function readCmd(input, vehicule, operator) {
 
-	// log cmd into blackbox array and send notification
-	let log = "initiating sequence: " + input + " from " + operator.toUpperCase() + " on :'' " + Math.round((new Date()).getTime() / 1000);
+	// create a log message for the vehicule blackbox to save
+	let date = new Date();
+	date.toISOString();
+	let log = "initiating sequence: " + input + " from " + operator.toUpperCase() + " on " + date;
+
+	// send it as notification and save it to corresponding array
 	console.log(log);
-	vehicule.blackBox.push(log);
 	hulla.send(log, "info");
+	vehicule.blackBox.push(log);
 
 	for (var i = 0; i < input.length; i++) {
 
@@ -189,6 +221,7 @@ function readCmd(input, vehicule, operator) {
 			moveBackward(vehicule);
 		} else {
 			console.log(value + " is not a valid input, please use (f)orward, (b)ackward, (r)ight or (l)eft.");
+			hulla.send(value + " is not a valid input, please use (f)orward, (b)ackward, (r)ight or (l)eft.");
 		}
 
 		// print current position when vehicule has executed it's last move 
@@ -198,10 +231,11 @@ function readCmd(input, vehicule, operator) {
 	}
 }
 
-// cmd from keyboad to online rover, set default if none
+// Process commands from keyboad to focused rover, set default if rover none has been selected through scenario button
 // https://stackoverflow.com/questions/1402698/binding-arrow-keys-in-js-jquery
 document.onkeydown = function (e) {
 	e = e || window.event;
+	// if a rover is currently
 	if (currentRover != null) {
 		switch (e.which || e.keyCode) {
 		case 37: // left
@@ -224,32 +258,34 @@ document.onkeydown = function (e) {
 			return; // exit this handler for other keys
 		}
 	} else {
-		alert("No rover was selected !\nConnecting on Rover Opportunity by default..");
-		currentRover = crafts[0]; // default Rover is the first of array.
-		document.dispatchEvent(event);
+		
+		// default Rover is the first available a given array. TODO dynamic array select (crafts, probes, shuttles..)
+		currentRover = defaultVehicule(crafts); 
+				
 	}
 	e.preventDefault(); // prevent the default action (scroll / move caret)
 };
 
 // ======================
 // GUI
-// basic grid setup, best results with square x^2 and x/10
+// basic grid setup
 var width = 250,
 	height = 250,
 	resolution = 25,
 	r = 25; // rover size
 
-// pull data (x, y, name) for crafts[i]
+// pull data (x, y, name) for crafts[i] and map it on map
 // https://stackoverflow.com/questions/16058791/d3-js-how-to-use-map-function
 crafts.map(function (d) {
 	return {
-		x: round(d.position.x * width, resolution),
-		y: round(d.position.y * height, resolution),
+		x: d.position.x * width,
+		y: d.position.y * height,
 		name: d.name,
+		_id: d._id,
 	};
 });
 
-// create random obstacles
+// create obstacles at random poditions 
 var obstacles = d3.range(3).map(function () {
 	return {
 		x: round(Math.random() * width, resolution),
@@ -258,50 +294,30 @@ var obstacles = d3.range(3).map(function () {
 });
 
 // update current used rover position on GUI
-function moveCircle(roverId, x, y) {
-	var circle = d3.selectAll("circle")
-		.filter(function (d, i) {
-			return i === roverId; // https://d3indepth.com/selections/
-		});
-	circle.transition()
+function moveCircle(x, y) {
+	
+	// select corresponding rover circle  // see filter or direct selection https://d3indepth.com/selections/ && https://stackoverflow.com/questions/20414980/d3-select-by-attribute-value
+	var selectedCircle = d3.select( "#vehicule" + currentRover._id ); 
+	selectedCircle.transition()
 		.duration(250)
 		.attr("cx", x * r)
 		.attr("cy", y * r)
 		.ease("easebounce");
 
-	// check if rover position equals an obstacle (TODO  add zone area sensitivity)
+	// check if rover position equals an obstacle on each move 
+	// TODO add zone area sensitivity to handle bigger obstacles
 	obstacles.forEach(function (element) {
 		if ((x == (element.x / r)) && (y == (element.y / r))) {
 
-			// notify, disconnect
-			hulla.send("Rover was destroyed ! ", "danger");
-			focusVehicule("disconnect");
-			document.dispatchEvent(event2)
-
-			// TODO REMOVE MARKER
-			var destroyedCircle = d3.selectAll("circle")
-				.filter(function (d, i) {
-					return i === roverId; // https://d3indepth.com/selections/
-				});
+			var destroyedCircle = d3.select( "#vehicule" + currentRover._id );
+			
+			// remove and disable circle and vehicule
 			destroyedCircle.remove();
-
+			destroyVehicule();
 		}
 	});
 
 }
-
-// change circle color according to rover's status
-document.addEventListener("roverSelection", function () {
-	var circle = d3.select("circle")
-		.classed("online-rover", true);
-}, false);
-
-// disconnect rover	and turn it red on event TODO disconnect from specific rover (multiplayer)
-document.addEventListener("roverDisconnected", function () {
-	var circle = d3.select("circle")
-		.classed("online-rover", false);
-	focusVehicule("disconnect");
-}, false);
 
 // math max is enforcing bounds of draggable rovers TODO HELP apply to user input
 function dragged(d) {
@@ -336,13 +352,14 @@ function round(p, n) {
 function initScenarii(arr) {
 
 	// TODO BUGGY HELP loop trhough array to create a list of available Rovers for User prompt according to JSON data
+	// TODO REFACTOR to assign  currentVehicule to chosen probe from availables 
 
 	// let list = []; 
 	// arr.forEach(function(i) {
 	// 	list.push(arr.join(i.name + ",\n "));
 	// });
 	// var probe = prompt("Please choose a probing vehicule:"+ list );
-
+	
 	var probe = prompt("Please choose a probing vehicule* :\n- 1 for Opportunity Rover\n- 2 for Dedication Rover", "1");
 	var operator = prompt("Please enter your name.");
 	var cmd = prompt("Please enter Rover's sequence* :\n Available commands are :\n- (f)orward\n- (b)ackward\n- (l)eft\n- (r)ight", "zrffrfflfrff");
@@ -370,6 +387,13 @@ function initScenarii(arr) {
 // do this stuff after page load
 $(document).ready(function () {
 
+	// data from distinct json file later
+	// var craftsfromJSON = jQuery.getJSON( "../data.json", function( data ) {
+	// 	console.log("data from file" + data);
+	// 	console.log("data from js" + crafts);
+	// 	return data;
+	// });
+
 	// create a canvas 
 	var svg = d3.select("#grid").append("svg")
 		.attr("width", width)
@@ -380,13 +404,9 @@ $(document).ready(function () {
 		.data(d3.range(1, width / resolution))
 		.enter().append("line")
 		.attr("class", "vertical")
-		.attr("x1", function (d) {
-			return d * resolution;
-		})
+		.attr("x1", function (d) { return d * resolution; })
 		.attr("y1", 0)
-		.attr("x2", function (d) {
-			return d * resolution;
-		})
+		.attr("x2", function (d) { return d * resolution; })
 		.attr("y2", height)
 		.each(function (d, i) {
 			var odd = i % 2 === 0;
@@ -401,40 +421,30 @@ $(document).ready(function () {
 		.enter().append("line")
 		.attr("class", "horizontal")
 		.attr("x1", 0)
-		.attr("y1", function (d) {
-			return d * resolution;
-		})
+		.attr("y1", function (d) { return d * resolution; })
 		.attr("x2", width)
-		.attr("y2", function (d) {
-			return d * resolution;
-		})
+		.attr("y2", function (d) { return d * resolution; })
 		.each(function (d, i) {
 			var odd = i % 2 === 0;
 			d3.select(this)
 				.style("stroke-width", odd ? null : 1.75); // thicker line each 2
 		});
 
-	// draw circle from  data
+	// draw circle from data
 	var circles = svg.selectAll("circle")
 		.data(crafts)
 		.enter().append("circle")
-		.attr("cx", function (d) {
-			return d.x;
-		})
-		.attr("cy", function (d) {
-			return d.y;
-		})
-		.attr("label", function (d) {
-			return d.name;
-		})
+		.attr("cx", function (d) { return d.x; }) // bind coordinates, id and name to circles
+		.attr("cy", function (d) { return d.y; })
+		.attr("id", function(d){ return ("vehicule" + d._id); })
+		.attr("label", function (d) { return d.name; })
 		.attr("r", r / 2)
-		.attr("transform", function (d) {
-			return "translate(" + (resolution / 2) + "," + (resolution / 2) + ")";
-		}) // bypass circle position offset
+		.attr("transform", function (d) {return "translate(" + (resolution / 2) + "," + (resolution / 2) + ")"; 	}) // bypass circle position offset
 		.classed("rover", true);
 	//.call(drag); // make them draggable 
 
 	// TODO ADD ORIENTATION ARROW TO ROVER... 
+
 	// circles.append("text")
 	// 	.data(crafts)
 	// 	.attr("class", "rover")
@@ -468,23 +478,20 @@ $(document).ready(function () {
 		})
 		.attr("font-family", "Arial")
 		.attr("font-size", "16px")
-		.attr("fill", "black");
-	//.attr("transform", function(d) { return "translate(" + round(d.position.x * width, resolution) + "," + round(d.position.y * width, resolution) + ")"; });
+		.attr("fill", "black")
 
 	var worries = svg.selectAll("obs")
 		.data(obstacles)
 		.enter().append("path")
 		.attr("d", d3.svg.symbol().type("triangle-up").size(r * 10))
 		//.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; }) // obstacles on grid crossings
-		.attr("transform", function (d) {
-			return "translate(" + (d.x + resolution / 2) + "," + (d.y + resolution / 2) + ")";
-		}) // bypass circle position offset
+		.attr("transform", function (d) { return "translate(" + (d.x + resolution / 2) + "," + (d.y + resolution / 2) + ")"; }) // bypass circle position offset
 		.style("fill", "grey");
 
 }); // end of window load
 
 // TODO ENFORCING BOUNDARIES https://bl.ocks.org/mbostock/1557377
-// ORIENTATION CLUES
+// ORIENTATION CLUES and text
 // TODO add a Zoom option https://bl.ocks.org/mbostock/6123708
 // TODO React + SocketIO/Firebase https://stackblitz.com/edit/react-rover
 
